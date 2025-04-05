61f8194c011f75071e7c8db146eaf77e7ae581b0
Jepi Oktamipa <jefyokta50@gmail.com>
2025-04-05 07:24:19 +0700

    citation and some tests

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,23 @@
+<?php
+
+
+use Jefyokta\Json2Tex\Citation;
+
+require_once __DIR__ . "/../../vendor/autoload.php";
+
+
+test("3 author",function(){
+
+    $result = Citation::formatAuthorName('Doe, John and Smith, Jane and Tanaka, Hiroshi');
+
+    expect($result)->toBe("Doe, Smith and Tanaka");
+
+});
+
+test("5+ author",function(){
+   
+
+    $result = Citation::formatAuthorName('Doe, John and Smith, Jane and Tanaka, Hiroshi and Lee, Anna and Kim, Min and Zhang, Wei');
+
+    expect($result)->toBe("Doe, Smith, Tanaka, Lee, Kim et al.");
+});

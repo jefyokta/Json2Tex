61bdfbe98e214796a4801b28e2fa9df735b63102
Jepi Oktamipa <jefyokta50@gmail.com>
2025-04-11 09:15:20 +0700

    test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,23 +1,13 @@
 <?php
 
-
-use Jefyokta\Json2Tex\Citation;
+use Jefyokta\Json2Tex\Converter;
 
 require_once __DIR__ . "/../../vendor/autoload.php";
 
 
-test("3 author",function(){
+test("converting to html", function () {
 
-    $result = Citation::formatAuthorName('Doe, John and Smith, Jane and Tanaka, Hiroshi');
+    $result = Converter::setContent(json_decode(file_get_contents(__DIR__ . "/../../ex.json"))->contents)->getHtml();
 
-    expect($result)->toBe("Doe, Smith and Tanaka");
-
-});
-
-test("5+ author",function(){
-   
-
-    $result = Citation::formatAuthorName('Doe, John and Smith, Jane and Tanaka, Hiroshi and Lee, Anna and Kim, Min and Zhang, Wei');
-
-    expect($result)->toBe("Doe, Smith, Tanaka, Lee, Kim et al.");
+    expect($result)->toBe(file_get_contents(__DIR__."/../../result.html"));
 });
